4ad5eb3ef68dd067a3602590519a2f73a0d1ba36
Kenny Kruger <doritoboy.ekk@gmail.com>
2023-06-20 19:30:30 -0700

    js for breed drop down (not complete)

--- script.js
+++ script.js
@@ -3,11 +3,22 @@
 //not quite right it's a concept
 const userAction = async () => 
 {
+    const breedLoc = document.getElementById("dog-breeds");
     const response = await fetch('https://dog.ceo/api/breeds/list/all');
     const myJson = await response.json(); //extract JSON from the http response
     // do something with myJson
-    var breedArray = JSON.parse(myJson);
-    return breedArray
+    let breedArray = JSON.parse(myJson);
+    
+    breedArray.array.forEach(element => 
+    {
+        i+=0
+        elem = document.createElement("option");
+        elem.id = 'myID' + i;
+        elem.innerHTML = element;
+        breedLoc.insertAdjacentElement('afterbegin',elem,document.body.childNodes[0]);
+       // < value="reeses">Reeses</option> 
+    });
+
 }
 
 
